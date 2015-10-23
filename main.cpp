95ab211c2ab8d98c6f564eb95fbb4063aa7d16c8
Shahkyan Sergey <df@mail.ru>
2015-10-23 15:39:22 +0300

    Fast Fibonacci.

--- main.cpp
+++ main.cpp
@@ -11,20 +11,47 @@ double sqr(double x)
 }
 int fib (int k)
 {
-    return k==1? 1: k==2? 2: fib(k-1)+fib(k-2);
+    return k<3 ? k : fib(k-1)+fib(k-2);
+}
+
+void fib2_(int k, int& ck, int& ck1)
+{
+   if (k <= 2)
+   {
+       ck = k;
+       ck1 = k - 1;
+    }
+   else
+   {
+     fib2_(k-1, ck1, ck);
+     ck += ck1;
+   }
+}
+
+int fib2(int k)
+{
+ int ck, ck1;
+ fib2_(k, ck, ck1);
+ return ck;
+
 }
 
 int main()
 
 {
-    cout << 5*3 << ' ' << 10 << endl;
-    cout << "Hello" << endl;
-    cout << sin(1) << endl;
+//    cout << 5*3 << ' ' << 10 << endl;
+//    cout << "Hello" << endl;
+//    cout << sin(1) << endl;
     cout << sqr (sqr(1.5)) << endl;
     QTime t;
     t.start();
     cout << fib (45) << endl;
     cout << " Time elapsed: " << t.elapsed() << " ms " << endl;
+
+    t.restart();
+    cout << fib2 (45) << endl;
+    cout << " Time elapsed: " << t.elapsed() << " ms " << endl;
+
     return 0;
 }
 
